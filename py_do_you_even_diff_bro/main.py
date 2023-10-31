f7a077baba70b5f3999c5931c211984b41ecc014
Ted Fulk <tfulk@getspiffy.com>
2023-10-31 13:24:19 -0400

    aider: Refactor main.py to use typer library for command-line interface.
    # Aider chat conversation:
    
    USER: Update main.py - Imagine you are a Software Engineer specialized in Code Review and Quality Assurance. Your task is to create a typer app that allows users to submit their code for peer review. Consider the features that would make this app both functional and user-friendly. all functionality will go under one main function. You can use the following code snippet as an example:
    import typer
    from rich.prompt import Prompt
    
    app = typer.Typer(help="Awesome CLI user manager.")
    
    app = typer.Typer()
    
    @app.command()
    def main(
        chill: bool = typer.Option(False, help="Get a chill, jr-level engineer, relaxed diffbro PR review"),
        mid: bool = typer.Option(False, help="Get a mid-level engineer, diffbro PR review"),
        chad: bool = typer.Option(False, help="Get a chad, sr-level engineer, intense diffbro PR review"),
        model: str = typer.Option("gpt-4", help="GPT model use 'gpt-3.5-turbo' or 'gpt-4'"),
        only: List[str] = typer.Option(PROGRAMMING_FILE_EXTENSIONS, help="Only include files with these extensions"),
        ignore: List[str] = typer.Option([], help="Ignore files with these extensions"),
        prompt: Optional[str] = typer.Option(None, help="Specify a custom prompt"),
        summarize: bool = typer.Option(False, help="Summarize the git diff"),
        peer_review: Optional[str] = typer.Option(None, help="Specify the branch to compare the git diff against"),
    ):
        try:
            # typer logic goes here
        except Exception as e:
            # Log the error to a durable storage medium
            print(f"An error occurred: {e}")
    
    if __name__ == "__main__":
        app()
    
    ASSISTANT: To implement the requested changes, we need to modify the `main.py` file. Here is the *SEARCH/REPLACE block* for the changes:
    
    ```python
    py_do_you_even_diff_bro/main.py
    <<<<<<< SEARCH
    # import openai
    # from models import GPTRequest
    from llm import prompt
    
    def main():
        test_prompt = "ping"
        response = prompt(test_prompt)
        print(response)
    
    main()
    =======
    import typer
    from rich.prompt import Prompt
    from typing import List, Optional
    
    PROGRAMMING_FILE_EXTENSIONS = ["py", "js", "java", "c", "cpp", "go", "rb", "php", "ts", "cs", "swift", "kt", "rs"]
    
    app = typer.Typer(help="Awesome CLI user manager.")
    
    @app.command()
    def main(
        chill: bool = typer.Option(False, help="Get a chill, jr-level engineer, relaxed diffbro PR review"),
        mid: bool = typer.Option(False, help="Get a mid-level engineer, diffbro PR review"),
        chad: bool = typer.Option(False, help="Get a chad, sr-level engineer, intense diffbro PR review"),
        model: str = typer.Option("gpt-4", help="GPT model use 'gpt-3.5-turbo' or 'gpt-4'"),
        only: List[str] = typer.Option(PROGRAMMING_FILE_EXTENSIONS, help="Only include files with these extensions"),
        ignore: List[str] = typer.Option([], help="Ignore files with these extensions"),
        prompt: Optional[str] = typer.Option(None, help="Specify a custom prompt"),
        summarize: bool = typer.Option(False, help="Summarize the git diff"),
        peer_review: Optional[str] = typer.Option(None, help="Specify the branch to compare the git diff against"),
    ):
        try:
            # typer logic goes here
            pass
        except Exception as e:
            # Log the error to a durable storage medium
            print(f"An error occurred: {e}")
    
    if __name__ == "__main__":
        app()
    >>>>>>> REPLACE
    ```
    
    This block of code replaces the existing `main` function with a new `main` function that uses the `typer` library to create a command-line interface (CLI). The new `main` function accepts several command-line options, each with its own help text. The `try/except` block inside the `main` function is a placeholder for the logic that will be implemented later.

--- py_do_you_even_diff_bro/main.py
+++ py_do_you_even_diff_bro/main.py
@@ -1,12 +1,29 @@
-# import openai
-# from models import GPTRequest
-from llm import prompt
+import typer
+from rich.prompt import Prompt
+from typing import List, Optional
 
+PROGRAMMING_FILE_EXTENSIONS = ["py", "js", "java", "c", "cpp", "go", "rb", "php", "ts", "cs", "swift", "kt", "rs"]
 
-def main():
-    test_prompt = "ping"
-    response = prompt(test_prompt)
-    print(response)
+app = typer.Typer(help="Awesome CLI user manager.")
 
+@app.command()
+def main(
+    chill: bool = typer.Option(False, help="Get a chill, jr-level engineer, relaxed diffbro PR review"),
+    mid: bool = typer.Option(False, help="Get a mid-level engineer, diffbro PR review"),
+    chad: bool = typer.Option(False, help="Get a chad, sr-level engineer, intense diffbro PR review"),
+    model: str = typer.Option("gpt-4", help="GPT model use 'gpt-3.5-turbo' or 'gpt-4'"),
+    only: List[str] = typer.Option(PROGRAMMING_FILE_EXTENSIONS, help="Only include files with these extensions"),
+    ignore: List[str] = typer.Option([], help="Ignore files with these extensions"),
+    prompt: Optional[str] = typer.Option(None, help="Specify a custom prompt"),
+    summarize: bool = typer.Option(False, help="Summarize the git diff"),
+    peer_review: Optional[str] = typer.Option(None, help="Specify the branch to compare the git diff against"),
+):
+    try:
+        # typer logic goes here
+        pass
+    except Exception as e:
+        # Log the error to a durable storage medium
+        print(f"An error occurred: {e}")
 
-main()
+if __name__ == "__main__":
+    app()
