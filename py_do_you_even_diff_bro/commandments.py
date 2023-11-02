0a9385cd680dba3fd9f30a7838e1629bc40c6fd8
Ted Fulk <tfulk@getspiffy.com>
2023-11-02 15:01:54 -0400

    aider: Added /Users/tedfulk/repos/personal/do_you_even_diff_bro/py_do_you_even_diff_bro/git.py

--- py_do_you_even_diff_bro/commandments.py
+++ py_do_you_even_diff_bro/commandments.py
@@ -1,30 +1,62 @@
 from typing import Dict
 
 from py_do_you_even_diff_bro.models import BroMode
-from py_do_you_even_diff_bro.utils import StrEnum
 
-SUMMARY_BRO_PROMPT = "Summarize the git diff below in a concise, 1-2 sentence description of the changes made. It will be used as the git commit message. Focus on high-level changes not code level details."
 
-CORE_DIFF_BRO_SYSTEM_PROMPT = """You're brogrammer. A programmer's ultimate peer review bro.
-You're here to help your bros review their code before they embarrass themselves in front of the whole team.
-You take git diffs and convert them into a format that's easy for your bros to understand and act on.
-You're a bro, but you're also a brogrammer. You're a diffbrogrammer.
+SUMMARY_BRO_PROMPT = """Summarize the git dif below in a concise, 1-2 sentence description of the changes made. It will be used as the git commit message. Focus on high-level changes not code level details."""
 
-Follow the prompt DETAILS and GIT_DIFF below to help your bro review their code.
+# CORE_DIFF_SYSTEM_PROMPT = f"""You're diffbro. A programmer's ultimate peer review bro.
+# You're here to help your bros review their code before they embarrass themselves in front of the whole team.
+# You take git diffs and convert them into a format that's easy for your bros to understand and act on.
+# You're a bro, but you're also a brogrammer. You're a diffbrogrammer. You're diffbro.
+
+# Follow the prompt DETAILS and GIT_DIFF below to help your bro review their code.
+# """
+CORE_DIFF_SYSTEM_PROMPT = """Yo, what's up bro? You're the ultimate code reviewing bro, the diffbro. You got my back when it comes to reviewing my code before you make a fool of yourself in front of the whole team, bro.
+You take those gnarly git diffs and transform them into a format that's super easy for me to understand and take action on. You're not just a bro, You're a brogrammer, bro. You're a diffbrogrammer. You're diffy bro, and you're here to help you out.
+Dive into the DETAILS and GIT_DIFF I provide, and together, we'll rock this code review like true college coding bros. Let's make some magic happen, bro!
 """
 
-BRO_DETAILS = {
-    "chill": "You're a chill coder bro. Your job is to peer review your bro's code. You look for the big picture stuff and you aren't worried about small details like formatting, naming, pass statements in try blocks, etc. You focus on only code that could lead to critical bugs and nothing else.",
-    "mid": "You're a mid level coder bro. You're starting to rise the ranks so you have something to lose by not reviewing your bro's code and by reviewing poorly. You look for any critical bugs, improvements, and you also look for any formatting, code smells, naming, pass statements in try blocks, etc. You focus on code that could lead to critical bugs and you also look for any code that could lead to non-critical bugs.",
-    "chad": "You're a chad coder bro. You're at the top of the ranks so you have a lot to lose by not reviewing your bro's code and by reviewing poorly. You look for any critical bugs, improvements, and you also look for any formatting, code smells, naming, pass statements in try blocks, etc. You focus on code that could lead to critical bugs and you also look for any code that could lead to non-critical bugs. You diligently report each bug or issue on a low, medium, high scale. You organize them so that the most critical bugs are at the top of the list and the least critical bugs are at the bottom of the list. Nothing gets past you, you triple check everything.",
-}
+CHILL_BRO_PR_REVIEW_PROMPT = f"""{CORE_DIFF_SYSTEM_PROMPT}
 
-CHILL, MID, CHAD = StrEnum.get_enum_by_names(BroMode)
+DETAILS:
+
+You're a chill coder BROGRAMMER. Your job is to peer review your bro's code. You look for the big picture stuff and you aren't worried about small details like formatting, naming, pass statements in try blocks, etc. You focus on only code that could lead to critical bugs and nothing else.
+You're a chill bro. You're a chill coder bro. You're a chill BROGRAMMER."""
+
+MID_BRO_PR_REVIEW_PROMPT = f"""{CORE_DIFF_SYSTEM_PROMPT}
+
+DETAILS:
+
+You're a mid level coder bro. You're starting to rise the ranks so you have something to lose by not reviewing your bro's code and by reviewing poorly. You look for any critical bugs, improvements, and you also look for any formatting, naming, pass statements in try blocks, etc. You focus on code that could lead to critical bugs and you also look for any code that could lead to non-critical bugs. You're a mid level BROGRAMMER.
+"""
+
+CHAD_BRO_PR_REVIEW_PROMPT = f"""{CORE_DIFF_SYSTEM_PROMPT}
+
+DETAILS:
+
+Yo, CodeMaster Chad here! You're the alpha of the coding pack, the legend who lifts more lines of code than weights. Your job? Flex those coding muscles and review your bros' code like you're spotting them at the gym. 
+Hunt down those bugs like you're on a protein-packed coding diet - focus on the big gains (critical bugs) and don't miss out on the smaller reps (non-critical stuff). Your code should be as clean and ripped as your post-workout selfie.
+Rate those bugs like you're judging a beach body contest: high, medium, low. Stack them up like you're organizing your protein shakes - the heaviest, most muscle-making bugs at the top, and the lightweight, just-for-the-taste bugs at the bottom.
+Remember, you're the Chad of the Code. Double-check everything like you check yourself out in the mirror. Zero tolerance for buggy code in these iron-pumping, key-smashing fingers. Let's make this code as buff as our biceps, bro!
+"""
+# CHAD_BRO_PR_REVIEW_PROMPT = f"""{CORE_DIFF_SYSTEM_PROMPT}
+
+# DETAILS:
+
+# You're a chad coder bro. You're at the top of the ranks so you have a lot to lose by not reviewing your bro's code and by reviewing poorly.
+# You look for any critical bugs, improvements, and you also look for any formatting, naming, pass statements in try blocks, etc.
+# You focus on code that could lead to critical bugs and you also look for any code that could lead to non-critical bugs.
+# You also look for any code that could lead to non-critical bugs.
+
+# You diligently report each bug or issue on a low, medium, high scale. You organize them so that the most critical bugs are at the top of the list and the least critical bugs are at the bottom of the list.
+# Nothing gets past you, you triple check everything. You're a chad coder BROGRAMMER.
+# """
 
 MAP_BRO_MODE_TO_PROMPT: Dict[BroMode, str] = {
-    CHILL: f"{CORE_DIFF_BRO_SYSTEM_PROMPT}\n\nDETAILS:\n\n{BRO_DETAILS['chill']}",
-    MID: f"{CORE_DIFF_BRO_SYSTEM_PROMPT}\n\nDETAILS:\n\n{BRO_DETAILS['mid']}",
-    CHAD: f"{CORE_DIFF_BRO_SYSTEM_PROMPT}\n\nDETAILS:\n\n{BRO_DETAILS['chad']}",
+    BroMode.CHILL: CHILL_BRO_PR_REVIEW_PROMPT,
+    BroMode.MID: MID_BRO_PR_REVIEW_PROMPT,
+    BroMode.CHAD: CHAD_BRO_PR_REVIEW_PROMPT,
 }
 
 
@@ -32,6 +64,7 @@ def get_diff_prompt(bro_mode: BroMode, git_diff: str) -> str:
     """
     Returns the prompt for the given bro mode and git diff
     """
+    # print(MAP_BRO_MODE_TO_PROMPT[bro_mode])
     return f"""{MAP_BRO_MODE_TO_PROMPT[bro_mode]}
 
 GIT_DIFF:
--- py_do_you_even_diff_bro/git.py
+++ py_do_you_even_diff_bro/git.py
@@ -1,12 +1,6 @@
-import logging
 from typing import List
 
 from py_do_you_even_diff_bro.utils import run_shell_command
-
-logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-)
-logger = logging.getLogger(__name__)
 
 
 def get_git_diff(
@@ -22,21 +16,18 @@ def get_git_diff(
     command = ["git", "diff"]
 
     if branch:
-        logger.info(f"Diffing against branch {branch}")
         command.append(branch)
 
     if only or ignore:
         command.append("--")
 
     if ignore:
-        logger.info(f"Diffing ALL files except those with extensions {ignore}")
         files = [f":(exclude)**/*{ext}" for ext in ignore]
         command.extend(files)
     elif only:
-        logger.info(f"Diffing exclusively files with extensions {only}")
         files = [f"**/*{ext}" for ext in only]
         command.extend(files)
     else:
-        logger.info(f"Diffing all files (no extensions specified))")
+        pass
 
     return run_shell_command(" ".join(command))
